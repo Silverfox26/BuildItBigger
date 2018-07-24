ec0e236abfeb3d649ed4993240b98853c6eb6c23
Daniel Penz <daniel.penz@gmx.de>
2018-07-24 09:35:04 +0200

    feat: Implement joke telling via java library

---
+++ jokelib/src/main/java/com/example/jokelib/Jokes.java
@@ -0,0 +1,25 @@
+package com.example.jokelib;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Jokes {
+
+    private List<String> jokes;
+
+    public Jokes() {
+        this.jokes = new ArrayList<>();
+        this.jokes.add("What do you call security guards working outside Samsung shops?\n\nA: Guardians of the Galaxy");
+        this.jokes.add("Which two letters in the alphabet are always jealous?\n\nA: NV");
+        this.jokes.add("Why could the lifeguard not save the hippie?\n\nA: He was too far out man!");
+        this.jokes.add("Why don't ants get sick?\n\nA: Because they have little antybodies.");
+        this.jokes.add("What's the difference between a hippo and a zippo?\n\nA: One is really heavy, and the other is a little lighter.");
+        this.jokes.add("Why do cows have hooves instead of feet?\n\nA: They lactose.");
+    }
+
+    public String getJoke() {
+        Random rand = new Random();
+        return jokes.get(rand.nextInt(jokes.size()));
+    }
+}
